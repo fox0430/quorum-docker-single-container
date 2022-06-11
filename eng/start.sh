ae3f996288e5cf60788f370b74a31696fcb4cd62
fox0430 <shuu.n@protonmail.com>
2022-06-11 22:26:12 +0900

    Upgrade Quorum to 22.4.3

--- eng/start.sh
+++ eng/start.sh
@@ -14,4 +14,4 @@ export QUORUM_CONSENSUS=raft
 
 geth --datadir /qdata/dd --nodiscover --verbosity 6 --raft --raftport 50400 \
   --rpc --rpcaddr 0.0.0.0 --rpcvhosts=* --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft --emitcheckpoints --port 30303 --allow-insecure-unlock \
-  --ws --wsaddr 0.0.0.0 --wsport 8546 --wsorigins '*' --wsapi personal,admin,db,eth,net,web3,miner,shh,txpool,debug
+  --ws --ws.addr 0.0.0.0 --ws.port 8546 --ws.origins '*' --ws.api personal,admin,db,eth,net,web3,miner,shh,txpool,debug
