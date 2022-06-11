08af5e1496c824e9e1093cee34cb2e47e5570a48
fox0430 <shuu.n@protonmail.com>
2022-06-11 23:17:12 +0900

    Add `--rpc.allow-unprotected-txs` option

--- eng/start.sh
+++ eng/start.sh
@@ -12,6 +12,7 @@ fi
 export PRIVATE_CONFIG=ignore
 export QUORUM_CONSENSUS=raft
 
-geth --datadir /qdata/dd --nodiscover --verbosity 6 --raft --raftport 50400 \
+geth --datadir /qdata/dd --nodiscover --verbosity 6 --rpc.allow-unprotected-txs \
+  --raft --raftport 50400 \
   --rpc --rpcaddr 0.0.0.0 --rpcvhosts=* --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft --emitcheckpoints --port 30303 --allow-insecure-unlock \
   --ws --ws.addr 0.0.0.0 --ws.port 8546 --ws.origins '*' --ws.api personal,admin,db,eth,net,web3,miner,shh,txpool,debug
